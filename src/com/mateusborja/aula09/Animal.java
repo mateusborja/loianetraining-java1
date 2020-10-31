317500eae17f68e9b259e1776a3e1bbdc592bce1
mateus.borja <mateus.borja@gmail.com>
2020-10-31 19:16:18 -0400

    polimorfismo - aula 09

---
+++ src/com/mateusborja/aula09/Animal.java
@@ -0,0 +1,16 @@
+//java polimorphism
+package com.mateusborja.aula09;
+
+public class Animal {
+	
+	private static String tipoDoAnimal;
+	private static double altura;
+	
+
+	public void falar() {
+
+	}
+	
+	
+
+}

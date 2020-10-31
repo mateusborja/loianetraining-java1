f0f3f05a8a363186f177bc1d71d121651bc7b0a9
mateus.borja <mateus.borja@gmail.com>
2020-10-31 17:22:23 -0400

    aula 07

---
+++ src/com/mateusborja/aula07/Erro01.java
@@ -0,0 +1,12 @@
+package com.mateusborja.aula07;
+
+public class Erro01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		
+	}
+
+}
---
+++ src/com/mateusborja/aula07/Erro03.java
@@ -0,0 +1,10 @@
+package com.mateusborja.aula07;
+
+public class Erro03 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Voce digitou: ");
+	}
+
+}

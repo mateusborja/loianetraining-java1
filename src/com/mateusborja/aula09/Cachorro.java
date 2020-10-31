e60938bfd30e4a03e285b37c8b519329c67fb4ff
mateus.borja <mateus.borja@gmail.com>
2020-10-31 19:16:31 -0400

    polimorfismo e herança - aula 09

---
+++ src/com/mateusborja/aula09/Cachorro.java
@@ -0,0 +1,11 @@
+//java polimorphism and inheritance
+package com.mateusborja.aula09;
+
+public class Cachorro extends Animal {
+	
+	public void falar() {
+		System.out.println("Au Au");
+
+	}
+
+}
---
+++ src/com/mateusborja/aula09/Gato.java
@@ -0,0 +1,10 @@
+package com.mateusborja.aula09;
+
+public class Gato extends Animal {
+	
+	public void falar() {
+		System.out.println("miau");
+
+	}
+
+}

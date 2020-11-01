22f435eeecf1b8bd95744e19a5d0df67c1eb4113
mateus.borja <mateus.borja@gmail.com>
2020-10-31 21:17:31 -0400

    polimorfismo - aula 09

--- src/com/mateusborja/aula09/Cachorro.java
+++ src/com/mateusborja/aula09/Cachorro.java
@@ -2,7 +2,7 @@
 package com.mateusborja.aula09;
 
 public class Cachorro extends Animal {
-	
+
 	public void falar() {
 		System.out.println("Au Au");
 
--- src/com/mateusborja/aula09/Gato.java
+++ src/com/mateusborja/aula09/Gato.java
@@ -3,7 +3,7 @@ package com.mateusborja.aula09;
 public class Gato extends Animal {
 	
 	public void falar() {
-		System.out.println("miau");
+		System.out.println("miau miau");
 
 	}
 

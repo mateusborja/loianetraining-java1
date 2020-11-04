4096ed69c035fc60958d86e3408402285b8b0afc
mateus.borja <mateus.borja@gmail.com>
2020-11-04 11:28:50 -0400

    aula 13 loiane

---
+++ src/com/mateusborja/java1/aula13/CurtoCircuito.java
@@ -0,0 +1,21 @@
+//java operadoresLogicos
+package com.mateusborja.java1.aula13;
+
+public class CurtoCircuito {
+	public static void main(String[] args) {
+
+		boolean verdadeiro = true;
+		boolean falso = false;
+
+		boolean resultado1 = falso & verdadeiro;
+		boolean resultado2 = falso && verdadeiro;
+
+		System.out.println(resultado1);
+		System.out.println(resultado2);
+
+		int resultado3 = 1 + 1 - 1 + 1 * 1 / 1; // pela precedencia multiplicacao depois divisao, depois soma, subtracao
+		System.out.println(resultado3);
+
+	}
+
+}

c3150985f8e3b6584becffb7678f0a93db9d65ec
mateus.borja <mateus.borja@gmail.com>
2020-11-05 17:21:00 -0400

    aula 15 - exercicio 19

---
+++ src/com/mateusborja/java1/aula15exercicios/Exer19.java
@@ -0,0 +1,18 @@
+//java 1 loiane exercicio 11 - aula 15
+package com.mateusborja.java1.aula15exercicios;
+
+import java.util.Scanner;
+
+public class Exer19 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print(" ");
+
+		sc.close();
+
+	}
+
+}

91b24013ab30eb218a850a950fd99bed3c654775
mateus.borja <mateus.borja@gmail.com>
2020-11-05 17:17:01 -0400

    aula 15 - exercicio 18

---
+++ src/com/mateusborja/java1/aula15exercicios/Exer18.java
@@ -0,0 +1,28 @@
+//java 1 loiane exercicio 11 - aula 15
+package com.mateusborja.java1.aula15exercicios;
+
+import java.util.Scanner;
+
+public class Exer18 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("digite um numero: ");
+
+		int num1 = sc.nextInt();
+
+		if (num1 % 2 == 0) {
+			System.out.println(num1 + " é um número par.");
+
+		} else {
+			System.out.println(num1 + " é um número impar.");	
+
+		}
+
+		sc.close();
+
+	}
+
+}

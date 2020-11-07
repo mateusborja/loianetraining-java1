c97ee8cb04a773a835b284a67f008b57ddebe8e5
mateus.borja <mateus.borja@gmail.com>
2020-11-07 19:16:29 -0400

    aula 15 - exercicio 22

---
+++ src/com/mateusborja/java1/aula15exercicios/Exer22.java
@@ -0,0 +1,51 @@
+//java 1 loiane exercicio 22 - aula 15
+package com.mateusborja.java1.aula15exercicios;
+
+import java.util.Scanner;
+
+public class Exer22 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("quantidade em Kg Morango: ");
+		double qtdeMorango = sc.nextDouble();
+
+		System.out.print("quantidade em Kg Maça: ");
+		double qtdeMaca = sc.nextDouble();
+
+		double precoKgMorango = 0;
+		double precoKgMaca = 0;
+
+		if (qtdeMorango <= 5) {
+			precoKgMorango = 2.5;
+
+		} else {
+			precoKgMorango = 2.2;
+
+		}
+
+		if (qtdeMaca <= 5) {
+			precoKgMaca = 1.8;
+
+		} else {
+			precoKgMaca = 1.5;
+
+		}
+
+		double precoTotalMorango = qtdeMorango * precoKgMorango;
+		double precoTotalMaca = qtdeMaca * precoKgMaca;
+		double precoParcial = precoKgMorango + precoTotalMaca;
+		double precoTotal = precoParcial;
+
+		if ((qtdeMorango + qtdeMaca) > 8 || precoParcial > 25.0) {
+			precoTotal = precoParcial - (precoParcial / 100) * 10.0;
+		}
+
+		System.out.printf("%nPreço Total R$ %.2f%n", precoTotal + ".");
+		sc.close();
+
+	}
+
+}

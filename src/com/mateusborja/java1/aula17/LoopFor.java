421aad143a34dcc45df7d4d6390e8f8830b35b02
mateus.borja <mateus.borja@gmail.com>
2020-11-09 13:24:42 -0400

    aula 17 - java for

---
+++ src/com/mateusborja/java1/aula17/LoopFor.java
@@ -0,0 +1,20 @@
+package com.mateusborja.java1.aula17;
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("valor de i = " + i);
+		}
+
+		System.out.println();
+
+		for (int j = 5; j >= 0; j--) {
+			System.out.println("valor de i = " + j);
+
+		}
+
+	}
+
+}

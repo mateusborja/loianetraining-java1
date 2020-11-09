af429c75e20183feed4ff034e5e7c55e2f343d8b
mateus.borja <mateus.borja@gmail.com>
2020-11-09 13:25:06 -0400

    for com mais de uma variavel

---
+++ src/com/mateusborja/java1/aula17/LoopForMaisVariaveis.java
@@ -0,0 +1,14 @@
+package com.mateusborja.java1.aula17;
+
+public class LoopForMaisVariaveis {
+
+	public static void main(String[] args) {
+
+		for (int i = 0, j = 10; i < j; i++, j--) {
+			System.out.println("i = " + i + "; j = " + j);
+
+		}
+
+	}
+
+}

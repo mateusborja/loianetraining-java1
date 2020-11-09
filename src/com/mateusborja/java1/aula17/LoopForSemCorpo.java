86de0bff3d5aabbeb9460f27a2643c5d169a877d
mateus.borja <mateus.borja@gmail.com>
2020-11-09 13:29:53 -0400

    java for sem corpo

---
+++ src/com/mateusborja/java1/aula17/LoopForSemCorpo.java
@@ -0,0 +1,15 @@
+package com.mateusborja.java1.aula17;
+
+public class LoopForSemCorpo {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+
+		for (int i = 0; i < 5; sum += i++);
+		
+		System.out.println("soma e igual a = " + sum);
+
+	}
+
+}

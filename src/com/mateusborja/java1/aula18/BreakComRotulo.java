11df61efdeebbb54268f62c3cd411bb739790aa6
mateus.borja <mateus.borja@gmail.com>
2020-11-09 14:35:24 -0400

    aula 18 - java break com rotulo

---
+++ src/com/mateusborja/java1/aula18/BreakComRotulo.java
@@ -0,0 +1,37 @@
+package com.mateusborja.java1.aula18;
+
+public class BreakComRotulo { // semelhante ao goto (nao usual em jav)
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 4; i++) {
+			rotulo1: {
+				rotulo2: {
+					rotulo3: {
+						if (i == 1) {
+							break rotulo1;
+						}
+						if (i == 2) {
+							break rotulo2;
+						}
+
+						if (i == 3) {
+							break rotulo3;
+						}
+
+						System.out.println("rotulo3");
+					}
+
+					System.out.println("rotulo2");
+
+				}
+
+				System.out.println("rotulo1");
+			}
+
+			System.out.println("\nvalor de i = " + i);
+		}
+
+	}
+
+}

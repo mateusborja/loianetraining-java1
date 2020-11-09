7dbb709abde2727b47ec1e62a77d44317489110c
mateus.borja <mateus.borja@gmail.com>
2020-11-09 14:54:43 -0400

    aula 20 - matrizes

---
+++ src/com/mateusborja/java1/aula20/MatrizNotaAlunos.java
@@ -0,0 +1,15 @@
+package com.mateusborja.java1.aula20;
+
+public class MatrizNotaAlunos {
+
+	public static void main(String[] args) {
+
+		double[][] notaAlunos = new double[30][4];
+
+		for (double[] temp : notaAlunos) {
+			System.out.println(notaAlunos); //end de memoria alocada
+
+		}
+	}
+
+}

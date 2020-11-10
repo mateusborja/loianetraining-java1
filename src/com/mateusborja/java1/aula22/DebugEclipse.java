bc25820100ed61a22484a4a390b090e580c1a5f0
mateus.borja <mateus.borja@gmail.com>
2020-11-09 20:21:59 -0400

    aula 22 - debug no eclipse

---
+++ src/com/mateusborja/java1/aula22/DebugEclipse.java
@@ -0,0 +1,16 @@
+package com.mateusborja.java1.aula22;
+
+public class DebugEclipse {
+
+	public static void main(String[] args) {
+
+		int notas[] = new int[3];
+		
+		for (int i = 0; i < 4; i++) {
+			notas[i] = i;
+
+		}
+
+	}
+
+}

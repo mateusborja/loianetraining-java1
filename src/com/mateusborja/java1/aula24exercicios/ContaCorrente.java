593cf1bbc1200ad2f8fc52ea686912936fe74cdd
mateus.borja <mateus.borja@gmail.com>
2020-11-10 13:44:40 -0400

    aula 24 exericicios - exercicio 05

---
+++ src/com/mateusborja/java1/aula24exercicios/ContaCorrente.java
@@ -0,0 +1,14 @@
+//aula 24 - execicio 05 - classe de definicao
+package com.mateusborja.java1.aula24exercicios;
+
+public class ContaCorrente {
+
+	String numero;
+	String agencia;
+
+	boolean especial;
+
+	double limiteEspecial;
+	double saldoContaCorrente;
+
+}

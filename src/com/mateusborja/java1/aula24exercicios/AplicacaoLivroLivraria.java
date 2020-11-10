64d6b0cbe114f02807369ea76b9b38fddd50200d
mateus.borja <mateus.borja@gmail.com>
2020-11-10 13:23:54 -0400

    aula 24 exercicios - exercicio 03

---
+++ src/com/mateusborja/java1/aula24exercicios/AplicacaoLivroLivraria.java
@@ -0,0 +1,27 @@
+//aula 24 - execicio 03
+package com.mateusborja.java1.aula24exercicios;
+
+public class AplicacaoLivroLivraria {
+	
+	
+	public static void main(String[] args) {
+		
+		LivroLivraria livro = new LivroLivraria();
+		
+		livro.nome = "Meu bem querer";
+		livro.autor = "Joao Nascimento";
+		livro.qtdePaginas = 300;
+		livro.isbn = "ERS1200";
+		livro.anoLancamento = 2015;
+		
+		livro.preco = 45.54;
+		
+	}
+	
+	
+	
+
+	
+	
+
+}
---
+++ src/com/mateusborja/java1/aula24exercicios/LivroLivraria.java
@@ -0,0 +1,13 @@
+//aula 24 - execicio 03
+package com.mateusborja.java1.aula24exercicios;
+
+public class LivroLivraria {
+
+	String nome;
+	String autor;
+	int qtdePaginas;
+	int anoLancamento;
+	String isbn;
+	double preco;
+
+}

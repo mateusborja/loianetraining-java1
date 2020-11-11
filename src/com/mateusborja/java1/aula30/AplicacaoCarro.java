3b30e75b248bf64fa2fce5cbd65e3e0cd5e9765d
mateus.borja <mateus.borja@gmail.com>
2020-11-11 17:40:23 -0400

    aula 30 classe teste - this (atributos, metodos e construtores)

---
+++ src/com/mateusborja/java1/aula30/AplicacaoCarro.java
@@ -0,0 +1,16 @@
+//java 1 loiana aula 30 (palavra this) - classe teste
+package com.mateusborja.java1.aula30;
+
+public class AplicacaoCarro {
+
+	public static void main(String[] a) {
+		
+		
+		Carro van = new Carro("Fiat", "Ducato");
+		System.out.println(van.numPassageiros);
+		
+		
+		
+	}
+
+}

fd9e2cd952d5dee4d3d9049458d3db9cf4a8481f
mateus.borja <mateus.borja@gmail.com>
2020-11-11 17:38:53 -0400

    aula 28 pacotes

---
+++ src/com/mateusborja/java1/aula28/Pacotes.java
@@ -0,0 +1,18 @@
+package com.mateusborja.java1.aula28;
+
+import com.mateusborja.java1.aula15exercicios.Exer10;
+import com.mateusborja.java1.aula24.Carro;
+
+public class Pacotes {
+	public static void main(String[] args) {
+		
+		
+		Carro carro;
+		Carro carro2;
+		
+		Exer10 exer10;
+		
+		
+	}
+
+}

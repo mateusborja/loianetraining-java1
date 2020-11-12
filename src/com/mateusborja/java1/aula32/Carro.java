c3f35675206ba06589a367a606aeac75f5cd292e
mateus.borja <mateus.borja@gmail.com>
2020-11-12 09:16:26 -0400

    Aula 32 metodos get e set

---
+++ src/com/mateusborja/java1/aula32/Carro.java
@@ -0,0 +1,80 @@
+//java 1 loiane - aula 32 (metodos get e set) - classe definicao
+package com.mateusborja.java1.aula32;
+
+public class Carro {
+
+	private String marca;
+	private String modelo;
+	private int numPassageiros;
+	private double capCombustivel;
+	private double consCombustivel;
+	private boolean seisMarchas;
+
+	public String getMarca() {
+		return this.marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getNumPassageiros() {
+		return numPassageiros;
+	}
+	
+	public void setNumPassageiros(int numPassageiros) {
+		this.numPassageiros = numPassageiros;
+	}
+	
+	public double getCapCombustivel() {
+		return capCombustivel;
+	}
+	
+	public void setCapCombustivel(double capCombustivel) {
+		this.capCombustivel = capCombustivel;
+	}
+	
+	public double getConsCombustivel() {
+		return consCombustivel;
+	}
+	
+	public void setConsCombustivel(double consCombustivel) {
+		this.consCombustivel = consCombustivel;
+	}
+
+
+	public boolean isSeisMarchas() {
+		return seisMarchas;
+	}
+	
+	public void setSeisMarchas(boolean seisMarchas) {
+		this.seisMarchas = seisMarchas;
+	}
+	
+	
+	
+
+
+
+
+	
+
+
+	
+	
+	
+	
+
+	
+	
+
+
+}

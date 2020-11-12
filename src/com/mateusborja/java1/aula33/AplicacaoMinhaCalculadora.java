3420a49e7aea6cbfbb57e9b0249be6f596c8273b
mateus.borja <mateus.borja@gmail.com>
2020-11-12 11:35:39 -0400

    aula 33 sobrecarga

--- src/com/mateusborja/java1/aula33/AplicacaoMinhaCalculadora.java
+++ src/com/mateusborja/java1/aula33/AplicacaoMinhaCalculadora.java
@@ -1,4 +1,4 @@
-//java 1 loiane - aula 32 (sobrecarga de metodos) - classe teste
+//java 1 loiane - aula 33 (sobrecarga de metodos) - classe teste
 package com.mateusborja.java1.aula33;
 
 public class AplicacaoMinhaCalculadora {
@@ -13,13 +13,14 @@ public class AplicacaoMinhaCalculadora {
 
 		int[] vet1 = { 1, 2, 3, 54, 34 };
 		int[] vet2 = { -1, -2, 13, 4, 4 };
+		
 		int somaVet1 = c1.soma(vet1);
 		int somaVet2 = c1.soma(vet2);
 
 		System.out.println("soma utilizando => soma(int num1, int num2) = " + sum1);
 		System.out.println("soma utilizando => soma(double num1, double num2) = " + sum2);
-		System.out.println("\nint[] vetorInteiros\n" + somaVet1 + "\nUtilizando o metodo soma.");
-		System.out.println("int[] vetorInteiros\n" + somaVet2 + "\nUtilizando o metodo soma.");
+		System.out.println("int[] vetorInteiros" + somaVet1 + "soma.");
+		System.out.println("int[] vetorInteiros" + somaVet2 + "\nUtilizando o metodo soma.");
 
 	}
 

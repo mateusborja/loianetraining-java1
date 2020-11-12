b5b5c746d75cbf936b44733788ec0888a3d21871
mateus.borja <mateus.borja@gmail.com>
2020-11-12 13:53:18 -0400

    aula 34 modificador de acesso static

---
+++ src/com/mateusborja/java1/aula34/MinhaCalculadora.java
@@ -0,0 +1,42 @@
+//java 1 loiane - aula 34 (modificador de acesso static) - classe definicao
+package com.mateusborja.java1.aula34;
+
+public class MinhaCalculadora {
+
+	private int[] vetInteiros;
+
+	public static int soma(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public double soma(double num1, double num2) {
+		return num1 + num2;
+	}
+
+	public int soma(int[] vetNum1) {
+		int total = 0;
+
+		for (int i = 0; i < vetNum1.length; i++) {
+			total += vetNum1[i];
+		}
+		return total;
+	}
+
+	public int soma(int[] vetNum1, int[] vetNum2) {
+		int num1Total = 0;
+		int num2Total = 0;
+
+		for (int i = 0; i < vetNum1.length; i++) {
+			num1Total += vetNum1[i];
+		}
+		for (int j = 0; j < vetNum2.length; j++) {
+			num2Total += vetNum2[j];
+		}
+		return num1Total + num2Total;
+	}
+
+	public void setVetInteiros(int[] vetInteiros) {
+		this.vetInteiros = vetInteiros;
+	}
+
+}

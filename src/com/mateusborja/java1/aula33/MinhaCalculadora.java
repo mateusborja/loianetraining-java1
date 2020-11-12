7e324cbfe7259cfab017eedf014ef64b944113f6
mateus.borja <mateus.borja@gmail.com>
2020-11-12 11:35:34 -0400

    aula 33 sobrecarga

--- src/com/mateusborja/java1/aula33/MinhaCalculadora.java
+++ src/com/mateusborja/java1/aula33/MinhaCalculadora.java
@@ -1,10 +1,10 @@
-//java 1 loiane - aula 32 (sobrecarga de metodos) - classe definicao
+//java 1 loiane - aula 33 (sobrecarga de metodos) - classe definicao
 package com.mateusborja.java1.aula33;
 
 public class MinhaCalculadora {
 
 	private int[] vetInteiros;
-	
+
 	public int soma(int num1, int num2) {
 		return num1 + num2;
 	}
@@ -13,17 +13,28 @@ public class MinhaCalculadora {
 		return num1 + num2;
 	}
 
-	public int soma(int[] vetorInteiros) {
+	public int soma(int[] vetNum1) {
 		int total = 0;
 
-		for (int i = 0; i < vetorInteiros.length; i++) {
-			total += vetorInteiros[i];
-
+		for (int i = 0; i < vetNum1.length; i++) {
+			total += vetNum1[i];
 		}
-
 		return total;
 	}
-	
+
+	public int soma(int[] vetNum1, int[] vetNum2) {
+		int num1Total = 0;
+		int num2Total = 0;
+
+		for (int i = 0; i < vetNum1.length; i++) {
+			num1Total += vetNum1[i];
+		}
+		for (int j = 0; j < vetNum2.length; j++) {
+			num2Total += vetNum2[j];
+		}
+		return num1Total + num2Total;
+	}
+
 	public void setVetInteiros(int[] vetInteiros) {
 		this.vetInteiros = vetInteiros;
 	}

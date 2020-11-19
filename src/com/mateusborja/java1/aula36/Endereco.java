f32526b1e2cc6a71850fbabacc44be02da57184e
Mateus Borja <mateus.borja@gmail.com>
2020-11-18 20:42:45 -0400

    classe endereco

---
+++ src/com/mateusborja/java1/aula36/Endereco.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+//java1 aula 36 - classe definicao
+
+package com.mateusborja.java1.aula36;
+
+public class Endereco {
+
+	private String rua;
+	private String numero;
+	private String cep;
+	private String cidade;
+	private String estado;
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+}

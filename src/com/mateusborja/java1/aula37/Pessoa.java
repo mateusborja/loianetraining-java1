f8add627841607646f909dc3b91e6ad5c1837272
Mateus Borja <mateus.borja@gmail.com>
2020-11-18 23:09:10 -0400

    herança, superclasse

---
+++ src/com/mateusborja/java1/aula37/Pessoa.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java1.aula37;
+
+public class Pessoa {
+
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String cpf;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}

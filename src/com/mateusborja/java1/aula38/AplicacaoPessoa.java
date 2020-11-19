f19a89dd1cd6cb4f25db7e02ef8455b5f656d69a
Mateus Borja <mateus.borja@gmail.com>
2020-11-19 15:09:45 -0400

    classe teste

---
+++ src/com/mateusborja/java1/aula38/AplicacaoPessoa.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java1.aula38;
+
+public class AplicacaoPessoa {
+
+	public static void main(String[] args) {
+
+		Aluno aluno = new Aluno("Joao Silva", "Rua 9, Casa 25", "xxx-xxx-xx-x", "supletivo");
+		Pessoa aluno1 = new Aluno();
+
+		aluno1.setNome("joao");
+		aluno1.setCpf("xxx.xxxx");
+	
+
+		if (aluno != null && aluno1 != null) {
+			System.out.println(aluno.getNome() + " e " + aluno1.getNome());
+		}
+
+		Aluno[] notas = new Aluno[1];
+	
+		
+		 
+		
+	
+
+
+		System.out.println(aluno.toString());
+
+	}
+
+}

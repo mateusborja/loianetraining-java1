7b849c8160af6cca8aca1412a838a7bfa1e2edb3
Mateus Borja <mateus.borja@gmail.com>
2020-11-18 20:25:16 -0400

    aula 36 relacionamento entre classes - classe contato

---
+++ src/com/mateusborja/java1/aula36/Contato.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+// java1 aula 36 loiane relação entre classe tem um tem muitos - classe definicao
+
+
+
+package com.mateusborja.java1.aula36;
+
+public class Contato {
+
+	private String nome;
+	private String endereco;
+	private String telefone;
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}

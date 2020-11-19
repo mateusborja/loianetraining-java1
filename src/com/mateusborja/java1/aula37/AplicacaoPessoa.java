f5b2b67b8d5e55b52a888d7ed462e3f47a347037
Mateus Borja <mateus.borja@gmail.com>
2020-11-18 23:09:23 -0400

    classe teste

---
+++ src/com/mateusborja/java1/aula37/AplicacaoPessoa.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java1.aula37;
+
+public class AplicacaoPessoa {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		Professor professor = new Professor();
+		
+		Pessoa aluno1 = new Aluno();
+		
+
+
+
+	}
+
+}

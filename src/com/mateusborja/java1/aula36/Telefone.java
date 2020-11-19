b57a69e64d7c9631d8798ef59e0e7ae970742487
Mateus Borja <mateus.borja@gmail.com>
2020-11-18 20:42:30 -0400

    classe telefone

---
+++ src/com/mateusborja/java1/aula36/Telefone.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+//java1 aula 36 - classe def
+
+package com.mateusborja.java1.aula36;
+
+public class Telefone {
+
+	private String tipo;
+	private String ddd;
+	private String numero;
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTipo() {
+		return tipo;
+
+	}
+
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+
+	public String getDdd() {
+		return ddd;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+}

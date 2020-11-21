b7db1dfce28bb7ce454f62855934a81e2a93c87a
Mateus Borja <mateus.borja@gmail.com>
2020-11-20 20:06:27 -0400

    aula 35 exercicio 02

---
+++ src/com/mateusborja/java1/aula35exercicios/Exer02.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+//java aula 35 exer02 somatorio de um numero
+//somatorio
+// 5 = 5 + 4 + 3 + 2 + 1 ou 5 + somatorio(4)
+// 4 = 4 + 3 + 2 + 1 ou 4 + somatorio(3)
+// 3 = 3 + 2 + 1 ou 3 + somatorio(2)
+// 2 = 2 + 1 ou 2 + somatorio(1)
+// 1 = 1 
+
+
+package com.mateusborja.java1.aula35exercicios;
+
+import java.util.Scanner;
+
+public class Exer02 {
+
+	public static int somatorio(int num) {
+
+		if (num == 0) {
+			return 1;
+		}
+
+		return num + somatorio(num - 1);
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("digite um numero: ");
+		int num = sc.nextInt();
+
+		System.out.print("somatorio de " + num + " é: " + somatorio(num) + " ");
+		
+		System.out.println();
+		System.out.println();
+		for (int i = 1; i < 10; i++) {
+			System.out.println("somatorio de " + i + " é: " + somatorio(i));
+			
+		}
+
+		sc.close();
+
+	}
+
+}

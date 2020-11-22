59517c57b7d047f76d1612670e42e9b78894c702
Mateus Borja <mateus.borja@gmail.com>
2020-11-22 17:50:40 -0400

    classe teste

---
+++ src/com/mateusborja/java1/aula40/AplicacaoPessoa.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//java aula 40 polimorfismo - classe teste
+package com.mateusborja.java1.aula40;
+
+public class AplicacaoPessoa {
+
+	public static void main(String[] args) {
+
+		Pessoa pessoa = new Pessoa();
+		Pessoa aluno = new Aluno();
+		Pessoa professor = new Professor();
+		
+
+		pessoa.setEndereco("Rua 1, num1");
+		aluno.setEndereco("Rua 2, num2");
+		professor.setEndereco("Rua 3, num3");
+		
+		System.out.println(pessoa.obterEquiquetaEndereco());
+		System.out.println(aluno.obterEquiquetaEndereco());
+		System.out.println(professor.obterEquiquetaEndereco());
+
+	}
+
+}

92f20aa6d50a304af908659ba10e1ad37fe38e0c
Mateus Borja <mateus.borja@gmail.com>
2020-11-22 18:55:08 -0400

    final em atributos e classes - aula 42

---
+++ src/com/mateusborja/java1/aula42/Constantes.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java1.aula42;
+
+public final class Constantes {
+
+	public static final String SITE_MATEUS_BORJA = "www.mateusborja.life";
+	public static final String SITE_BORJAPHOTOS = "www.borjaphotos.com";
+	public static final String SITE_ALEX_BORJA = "www.alexborja.com";
+
+}

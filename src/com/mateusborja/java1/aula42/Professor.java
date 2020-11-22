7629298a8df19af62294dd8ba94ab127b8dfc5fa
Mateus Borja <mateus.borja@gmail.com>
2020-11-22 18:53:29 -0400

    aula 42 palavra chave final e Constantes

---
+++ src/com/mateusborja/java1/aula42/Professor.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//java 1 aula 42 palavra chave final - classe def
+package com.mateusborja.java1.aula42;
+
+public class Professor {
+
+	private String nomeCurso;
+	private double salario;
+
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public double calcularSalarioLiquido() {
+		// TODO
+		return 0;
+	}
+
+}

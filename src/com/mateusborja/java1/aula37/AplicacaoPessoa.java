cd5f1911c8185ee4cecafbbf0571fe6aa239127a
Mateus Borja <mateus.borja@gmail.com>
2020-11-23 10:18:07 -0400

    classe teste aula 37

--- src/com/mateusborja/java1/aula37/AplicacaoPessoa.java
+++ src/com/mateusborja/java1/aula37/AplicacaoPessoa.java
@@ -9,14 +9,18 @@ package com.mateusborja.java1.aula37;
 public class AplicacaoPessoa {
 
 	public static void main(String[] args) {
-		
+
 		Aluno aluno = new Aluno();
 		Professor professor = new Professor();
-		
+
 		Pessoa aluno1 = new Aluno();
-		
 
+		double notas[] = { 10, 8, 9.5 };
 
+		aluno.setCurso("Java Developer");
+		aluno.setNotas(notas);
+
+		System.out.println(aluno.obterEtiqueta());
 
 	}
 

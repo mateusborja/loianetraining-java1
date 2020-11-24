013a75326ecda93c64a85d0eb31e362008e492bc
Mateus Borja <mateus.borja@gmail.com>
2020-11-24 11:50:39 -0400

    classe mamifero

---
+++ src/com/mateusborja/java1/aula44/Mamifero.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//java 1 loiane aula 44 - interface - classe def
+
+package com.mateusborja.java1.aula44;
+
+public abstract class Mamifero extends Animal {
+
+	abstract void amamentar();
+
+}

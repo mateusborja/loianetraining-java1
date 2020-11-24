6c5469767e3f61441edc9c77895686a77534da9f
Mateus Borja <mateus.borja@gmail.com>
2020-11-24 12:44:02 -0400

    aula 44 - interface classe cavalo

---
+++ src/com/mateusborja/java1/aula44/Cavalo.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//java 1 loiane aula 44 - interface - classe def
+package com.mateusborja.java1.aula44;
+
+public class Cavalo extends Mamifero implements AnimalDomesticado {
+
+	@Override
+	public String toString() {
+		String s = super.toString();
+
+		return s;
+	}
+
+	@Override
+	public void alimentar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void levarVeterinario() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void chamarVeterinario() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void amamentar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void emitirSom() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

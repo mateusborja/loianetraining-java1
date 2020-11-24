6b96e9d2c39d9c6cfb1574db605575975d7b4252
Mateus Borja <mateus.borja@gmail.com>
2020-11-24 11:47:29 -0400

    classe ave subclasse

---
+++ src/com/mateusborja/java1/aula44/Ave.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java1.aula44;
+
+public abstract class Ave extends Animal  {
+	
+	abstract void voar();
+
+}

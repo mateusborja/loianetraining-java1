319af4871feec74a810860a10d358186ce791fd8
Mateus Borja <mateus.borja@gmail.com>
2020-11-24 12:45:27 -0400

    interfaces - aula 44

---
+++ src/com/mateusborja/java1/aula44/AnimalDomesticado.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//java 1 loiane aula 44 - interface - interface
+package com.mateusborja.java1.aula44;
+
+public interface AnimalDomesticado {
+	
+	void alimentar();
+	void levarVeterinario();
+	void chamarVeterinario();
+
+}
---
+++ src/com/mateusborja/java1/aula44/AnimalEstimacao.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//java 1 loiane aula 44 - interface - interface
+package com.mateusborja.java1.aula44;
+
+public interface AnimalEstimacao  {
+	
+	void brincar();
+	void levarPassear();
+	
+	
+
+}

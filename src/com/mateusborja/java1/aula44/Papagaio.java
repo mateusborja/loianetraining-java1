954ef991f2a66ed11f1262c6f8b3cf719ed70837
Mateus Borja <mateus.borja@gmail.com>
2020-11-24 12:44:21 -0400

    aula 44 - interface classe papagaio

---
+++ src/com/mateusborja/java1/aula44/Papagaio.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//java 1 loiane aula 44 - interface - classe def
+
+package com.mateusborja.java1.aula44;
+
+public class Papagaio extends Ave {
+
+	@Override
+	void voar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void emitirSom() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+}

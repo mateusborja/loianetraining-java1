ab166b42e7148477ad1ace63af405e7c55b251e5
Mateus Borja <mateus.borja@gmail.com>
2020-11-24 11:47:16 -0400

    classe animal super classe

---
+++ src/com/mateusborja/java1/aula44/Animal.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+//java 1 loiane aula 44 - interface - classe def
+package com.mateusborja.java1.aula44;
+
+public abstract class Animal {
+	
+	private String nome;
+	
+	abstract void emitirSom();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}

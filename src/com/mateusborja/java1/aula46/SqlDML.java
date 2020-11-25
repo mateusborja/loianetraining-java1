13893da12168edaf93c0d5a984c0f9fcd89d7810
Mateus Borja <mateus.borja@gmail.com>
2020-11-25 14:50:45 -0400

    interface SqlDML

---
+++ src/com/mateusborja/java1/aula46/SqlDML.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java1.aula46;
+
+public interface SqlDML {
+	
+	void select(String query);
+	void insert(String query);
+	void update(String query);
+	void delete(String query);
+
+}

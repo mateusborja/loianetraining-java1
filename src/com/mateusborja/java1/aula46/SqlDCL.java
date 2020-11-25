b1da8001ce4dde2ee1d7eadf4b1558de1198f4e3
Mateus Borja <mateus.borja@gmail.com>
2020-11-25 14:49:54 -0400

    interface SqlDCL aula 46 - interfaces

---
+++ src/com/mateusborja/java1/aula46/SqlDCL.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java1.aula46;
+
+public interface SqlDCL {
+
+	void grant(String access);
+	void revoke(String access);
+
+}

157abc6807919a6310e72e39dacef9dfe7f1d207
mateusborja <mateusborja@10.0.0.101>
2020-11-25 00:27:12 -0400

    classe teste aula 45 casting e operador instanceOf

--- src/com/mateusborja/java1/aula45/AplicacaoPessoa.java
+++ src/com/mateusborja/java1/aula45/AplicacaoPessoa.java
@@ -11,26 +11,29 @@ public class AplicacaoPessoa {
 	public static void main(String[] args) {
 
 		Aluno aluno = new Aluno();
-		aluno.setNome("Joao");
-		aluno.setCurso("Java Developer");
-		double notas1[] = { 10, 8, 7.5 };
-		aluno.setTelefone("99828 4828");
-		aluno.setEndereco("Rua Armando, 25");
+		aluno.setNome("Joao Honorio");
+		aluno.setCurso("Desenvolvimento Java Developer");
+		aluno.setTelefone("92 99828 4828");
+		aluno.setEndereco("Rua Armando, 25, Bairro Centro");
+		double notas1[] = { 10, 8, 7.5, 10, 8.0 };
 		aluno.setNotas(notas1);
 		System.out.println(aluno);
 
 		Pessoa alunoPessoa = aluno; // upcasting
 		Pessoa aluno2 = (Pessoa) new Aluno(); // convencao java Class objetc = new (Class) object 2 - casting manual
-		Pessoa aluno3 = new Pessoa();
-		
+
+		Pessoa alunoPessoa1 = aluno;
+
 		System.out.println(alunoPessoa);
-		
+		System.out.println(alunoPessoa1);
 
 		// Pessoa aluno1 = (Pessoa) aluno4;
 
 		Pessoa pessoa = new Pessoa();
 		Aluno aluno5 = new Aluno();
 		Professor prof = new Professor();
+		
+		
 
 		if (pessoa instanceof Pessoa) {
 			System.out.println("É do tipo Pessoa.");
@@ -48,6 +51,13 @@ public class AplicacaoPessoa {
 			System.out.println("É do tipo Professor.");
 		} else {
 			System.out.println("Váriavel \"pessoa\" não é do tipo Professor");
+		}
+
+		if (aluno2 instanceof Aluno) {
+			System.out.println("É do tipo Aluno.");
+
+		} else {
+			System.out.println("Variável \"aluno2 \" não é do Tipo Aluno");
 		}
 
 	}

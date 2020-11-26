a28cd0228be9da73c34aa53aa7e0eae30b8c7b91
Mateus Borja <mateus.borja@gmail.com>
2020-11-26 14:30:19 -0400

    aula 46 exercicios - heranca, interface, polimorfismo, operador instanceOf, upcasting, downcasting

---
+++ src/com/mateusborja/java1/aula46exercicios/FiguraGeometrica.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.java1.aula46exercicios;
+
+public class FiguraGeometrica {
+
+	private String nome;
+	private String cor;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+}

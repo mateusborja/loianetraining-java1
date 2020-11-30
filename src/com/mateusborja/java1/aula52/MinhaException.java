cec401929b47fa587bebcc16fbeaff5d2edc1f82
Mateus Borja <mateus.borja@gmail.com>
2020-11-30 15:16:56 -0400

    aula 52 exception personalizada

--- src/com/mateusborja/java1/aula52/MinhaException.java
+++ src/com/mateusborja/java1/aula52/MinhaException.java
@@ -3,15 +3,15 @@
  * @author Mateus Borja // UX Designer • 3D Artist • Developer
  * at www.mateusborja.life
  */
-
+//aula 52 exception personalizada - classe def e teste
 package com.mateusborja.java1.aula52;
 
 public class MinhaException {
 
 	public static void main(String[] args) {
 
-		int[] arr1 = { 4, 8, 5, 16, 32, 21, 64, 128 };
-		int[] arr2 = { 2, 0, 4, 8, 0, 2, 3 };
+		int[] arr1 = { 4, 8, 16, 21, 32, 64, 128 };
+		int[] arr2 = { 2, 0, 4, 8, 0 };
 
 		for (int i = 0; i < arr1.length; i++) {
 			try {
